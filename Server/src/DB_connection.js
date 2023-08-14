7b4fa0dfb5c54740f1b5425b1a63920b4c6e9df3
ALauraOliva <andrea_carol00@hotmail.com>
2023-08-14 14:08:20 -0500

    deploy server

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DATABASE_URL } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, DATABASE_URL } = process.env;
 const Character = require('./models/Character')
 const Favorite = require('./models/Favorite')
+
+// const sequelize = new Sequelize( `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
+// { logging: false, native: false }
+// );
 
 const sequelize = new Sequelize( DATABASE_URL,
 { logging: false, native: false }
